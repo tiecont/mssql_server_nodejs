1a097fd2e9750f74de12887e0110392ff8475e3c
NodeOf Ties <ties.node@outlook.com>
2024-04-16 05:39:31 +0700

    config

--- config/sqlConnection.js
+++ config/sqlConnection.js
@@ -1,7 +1,7 @@
 import { Sequelize } from '@sequelize/core';
 import { MsSqlDialect } from '@sequelize/mssql';
 
-export const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   dialect: 'mssql',
     server: 'admin-pc',
     dialectModulePath: 'sequelize-msnodesqlv8',
@@ -9,3 +9,21 @@ export const sequelize = new Sequelize({
         connectionString: 'Data Source=Admin-PC;Initial Catalog=Tiecont_Server;Integrated Security=True;Trust Server Certificate=True'
     }
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('connected...')
+    })
+    .catch(err => console.log(err))
+const db = {}
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+db.products = require('./products')(sequelize, DataTypes)
+db.user = require('./user')(sequelize, DataTypes)
+
+db.sequelize.sync({ force: false })
+    .then(() => {
+        console.log('connected to SSMS')
+    })
+
+export default db

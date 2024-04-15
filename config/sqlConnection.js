e39ea57105c47e579a9ccf32b68281dfd85531f4
tiecont <sysendev.ties@gmail.com>
2024-04-15 20:44:16 +0700

    config

--- config/sqlConnection.js
+++ config/sqlConnection.js
@@ -2,15 +2,10 @@ import { Sequelize } from '@sequelize/core';
 import { MsSqlDialect } from '@sequelize/mssql';
 
 export const sequelize = new Sequelize({
-  dialect: MsSqlDialect,
-  server: 'Administrator',
-  port: 1433,
-  database: 'database',
-  authentication: {
-    type: 'Database Engine',
-    options: {
-      userName: 'Administrator',
-      password: process.env.DB_PASSWORD,
-    },
-  },
+  dialect: 'mssql',
+    server: 'admin-pc',
+    dialectModulePath: 'sequelize-msnodesqlv8',
+    dialectOptions: {
+        connectionString: 'Data Source=Admin-PC;Initial Catalog=Tiecont_Server;Integrated Security=True;Trust Server Certificate=True'
+    }
 });
